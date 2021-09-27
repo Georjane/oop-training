db31fd77469b27baee0e3383cababd988adad5dd
Amine_Zerradi <dasileker@gmail.com>
2021-09-27 12:25:38 +0100

    correted rubocop errors

--- lib/main.rb
+++ lib/main.rb
@@ -75,15 +75,13 @@ class App
   end
 
   def list_rentals_for_person_id
-    print "ID of person: "
-    # *If needed, ask for parameters for the option.
+    print 'ID of person: '
     id = gets.chomp.to_i
 
-    rentals = @rentals.filter {|rental| rental.person.id == id}
-    puts "Rentals:"
+    rentals = @rentals.filter { |rental| rental.person.id == id }
+    puts 'Rentals:'
     rentals.each do |rental|
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
     end
   end
-
 end
