bb7df5fdc6e65d7050db590d325c78a58083b57d
Amine_Zerradi <dasileker@gmail.com>
2021-09-27 12:37:59 +0100

    add option for the user

--- lib/main.rb
+++ lib/main.rb
@@ -51,6 +51,17 @@ class App
     puts 'Person created successfully'
   end
 
+  def create_book
+    print "Title: "
+    title = gets.chomp
+
+    print "Author: "
+    author = gets.chomp
+
+    @books << Book.new(title, author)
+    puts "Book created successfully"
+  end
+
   def create_rental
     puts 'Select a book from the following list by number'
     @books.each_with_index do |book, index|
@@ -74,7 +85,7 @@ class App
     puts 'Rental created successfully'
   end
 
-  def list_rentals_for_person_id
+  def list_of_rentals_for_person_id
     print 'ID of person: '
     id = gets.chomp.to_i
 
@@ -87,7 +98,43 @@ class App
 end
 
 def main
-  
+  app = App.new()
+
+  response = nil
+
+
+  puts "Welcome to School Library App!\n\n"
+
+  while response != "7"
+    puts "Please choose an option by entering a number:"
+    puts "1 - List all books"
+    puts "2 - List all people"
+    puts "3 - Create a person"
+    puts "4 - Create a book"
+    puts "5 - Create a rental"
+    puts "6 - List all rentals for a given person id"
+    puts "7 - Exit"
+
+    response = gets.chomp
+
+    case response
+    when "1"
+      app.list_of_books()
+    when "2"
+      app.list_of_people()
+    when "3"
+      app.create_person()
+    when "4"
+      app.create_book()
+    when "5"
+      app.create_rental()
+    when "6"
+      app.list_of_rentals_for_person_id()
+    when "7"
+      puts "Thank you for using this app!"
+    end
+    puts "\n"
+  end
 end
 
 main()
