44853599ae834628d2beed0f769a1c0442797381
Georjane <pzgeorjane@gmail.com>
2021-09-27 06:23:09 +0100

    Setup linters

--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -1,19 +1,21 @@
-require './corrector.rb'
+# rubocop:disable Style/OptionalBooleanParameter
+# rubocop:disable Naming/PredicateName
+require './corrector'
 
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
-    @corrector = Corrector.new()
+    @corrector = Corrector.new
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?()
+    @parent_permission || is_of_age?
   end
 
   def validate_name
@@ -21,7 +23,11 @@ class Person
   end
 
   private
+
   def is_of_age?
     @age >= 18
   end
 end
+
+# rubocop:enable Style/OptionalBooleanParameter
+# rubocop:enable Naming/PredicateName
