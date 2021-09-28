3304e81a10707a221ad32d6bf5e2c4c4282510ba
Georjane <pzgeorjane@gmail.com>
2021-09-28 18:55:58 +0100

    Linter setup

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+git_source(:github) { |_repo_name| 'https://github.com/oop-training' }
+
+# gem "rails"
+
+gem 'rubocop', '>= 1.0', '< 2.0'
--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -1,19 +1,19 @@
-require './corrector.rb'
+require './corrector'
 
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
-    @corrector = Corrector.new()
+    @corrector = Corrector.new
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?()
+    @parent_permission || is_of_age?
   end
 
   def validate_name
@@ -21,6 +21,7 @@ class Person
   end
 
   private
+
   def is_of_age?
     @age >= 18
   end
